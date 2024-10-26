cecf8f2b636ab4b32df58bf35d062637177ee590
AkiMadie16 <madie86@gmail.com>
2024-10-26 20:34:28 +1030

    commited other files

---
+++ Readcsvfile.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Read data from a CSV file
+data = pd.read_csv('example.csv')
+
+# Display the first few rows of the data
+print(data.head())
---
+++ first.py
@@ -0,0 +1,25 @@
+# let's see how to print data in the console
+# we use print function 
+# we need to write a word inside quotes which called a string
+# we don't need the quotes when we declare a number i.e integer 
+
+
+print("Hello")
+print(24)
+
+# let's see how we declare variables 
+#  check the type of a variable
+
+# Example
+name = "Alice"  # String
+age = 28        # Integer
+height = 5.7    # Float
+is_analyst = True  # Boolean
+
+#let's see how these variables print in the console
+
+print(name)
+print(type(name)) 
+# Output: <class 'str'>
+print(type(is_analyst))
+# Output: <class 'bool'>
